dd2cc38475e3de3ab5d6142ebb4d3b7d9106e139
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-10-04 12:01:06 -0700

    To print all possible different combinations of two digits.

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+for i in range(9):
+    for n in range(i + 1, 10):
+        print("{:d}{:d}".format(i, n), end='')
+
+        if (i == 8) and (n == 9):
+            continue
+
+        print(", ", end='')
+
+print()
