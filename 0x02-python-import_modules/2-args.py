602cbb626401bf8d366ce64dcf60cb2747d80d5b
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-10-05 09:20:33 -0700

    To print the content of sys.argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    size = len(argv) - 1
+    if size > 0:
+        if size == 1:
+            print("{:d} argument:".format(size))
+        else:
+            print("{:d} arguments:".format(size))
+
+        for index in range(1, size + 1):
+            print("{:d}: {:s}".format(index, argv[index]))
+    else:
+        print("0: arguments.")
