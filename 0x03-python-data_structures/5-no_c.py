1158672762e5e43e88bd328ad4390c72631ffb41
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-10-08 12:52:40 -0700

    Fixed Bug: Issues with the if statement

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,7 +4,7 @@ def no_c(my_string):
     size = len(my_string)
     new_string = ""
     for idx in range(size):
-        if my_string[idx] == "c" or my_string[idx] == "C":
+        if my_string[idx] != "c" or my_string[idx] != "C":
             new_string = new_string + my_string[idx]
 
     return new_string
