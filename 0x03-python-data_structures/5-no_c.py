ebef16a4f76db22bab9c633875d28e68991dc395
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-10-08 13:14:19 -0700

    Fixed Bug: Issues with the if statement

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
     """Remove the characters C and c from a list and return the new list"""
-    size = len(my_string)
     new_string = ""
-    for idx in range(size):
-        if my_string[idx] != "c" or my_string[idx] != "C":
-            new_string = new_string + my_string[idx]
+    for value in my_string:
+        if value != "c" and value != "C":
+            new_string = new_string + value
 
     return new_string
