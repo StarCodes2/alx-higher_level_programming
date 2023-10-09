336816b2c7cc07e2f1656db6fe4744a3cab56d19
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-10-09 00:41:08 -0700

    To find the biggest number in a list of integers

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     """Find the biggest number from the numbers in a list"""
-    max_num = 0
-    for num in my_list:
-        if num > max_num:
-            max_num = num
+    if len(my_list) != 0:
+        max_num = 0
+        for num in my_list:
+            if num > max_num:
+                max_num = num
+    else:
+        max_num = None
 
     return max_num
