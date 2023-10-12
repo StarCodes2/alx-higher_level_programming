3652d3127bde3eff591a2d15ee788d090239d918
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-10-12 04:15:51 -0700

    To compute the weighted average of a list of integer tuples

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,6 +2,10 @@
 def weight_average(my_list=[]):
     """Compute the weighted average of all integer tuple in a list"""
     a, b = 0, 0
+
+    if my_list is []:
+        return 0
+
     for value in my_list:
         a += value[0] * value[1]
         b += value[1]
