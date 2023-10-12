687e04d8de8fa79d9398820304a72055911c0b90
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-10-12 03:37:09 -0700

    Edited to meet pycodestyle requirements

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     """Find the key with the highest value in a dictionaty of integer values"""
     key, h_value = None, 0
-    if a_dictionary != None:
+    if a_dictionary is not None:
         for i, value in a_dictionary.items():
             if value > h_value:
                 h_value = value
