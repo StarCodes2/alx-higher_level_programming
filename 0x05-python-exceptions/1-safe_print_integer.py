aa921411277f3cce39391ddbac5c307d785bbb63
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-10-23 12:33:59 -0700

    To print an integer with "{:d}".format().

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    """prints an integer with "{:d}".format()."""
+    prt = True
+    try:
+        print('{:d}'.format(value))
+    except ValueError:
+        prt = False
+
+    return prt
