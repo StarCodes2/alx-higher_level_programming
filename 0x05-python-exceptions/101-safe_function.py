b917929311ad4be0f365357c25186704c3b0ca02
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-10-23 16:38:49 -0700

    To execute a function safely.

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,7 +5,7 @@ import sys
 def safe_function(fct, *args):
     """executes a function safely."""
     try:
-        result = fct(args[0], args[1])
+        result = fct(*args)
         return result
     except Exception as e:
         sys.stderr.write('Exception: {}\n'.format(e))
