ec707aa0ed24582313837ce3c165fa874c6e3e0c
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-10-24 18:55:17 -0700

    Class square with a private instance attribute size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define class square"""
+
+
+class Square:
+    """a class with an init size attribute."""
+    def __init__(self, size=0):
+        """Initializes the size attribute."""
+        self.__size = size
