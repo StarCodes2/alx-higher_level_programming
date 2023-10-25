4aa5ebae9f606e5ea431437803cae13ef4b3f75b
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-10-25 12:42:00 -0700

    Coordinate of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -35,13 +35,12 @@ class Square:
     @position.setter
     def position(self, value):
         """modifies the content of position."""
-        if (isinstance(value, tuple) and
-                len(value) == 2 and
-                all(isinstance(num, int) for num in value) and
-                all(num >= 0 for num in value)):
-            self.__position = value
-        else:
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
             raise TypeError('position must be a tuple of 2 positive integers')
+        self.__position = value
 
     def area(self):
         """returns the area of a square."""
