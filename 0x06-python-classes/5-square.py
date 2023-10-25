9686d36f692a4c2893c8e312dc2d3877779d99e8
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-10-25 01:07:32 -0700

    added a method to print a square with # characters

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -32,9 +32,10 @@ class Square:
 
     def my_print(self):
         """prints a square with # characters."""
+        if self.__size == 0:
+            print()
+
         for i in range(self.__size):
             for j in range(self.__size):
                 print('#', end="")
             print()
-
-        print()
