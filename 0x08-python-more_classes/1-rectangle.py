2daf5a7e9b5fe6c2f98fe7cce99167b7c49d2e17
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-10-30 13:18:09 -0700

    Define a class with private attributes

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""defines class called Rectangle."""
+
+
+class Rectangle:
+    """Class defunes two private instance attribute."""
+    def __init__(self, width=0, height=0):
+        """Intialized with optional width and height."""
+        self.__height = height
+        self.__width = width
+
+    @property
+    def height(self):
+        """returns the value of the private attribute height."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """sets the value of the private attribute height."""
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
+
+    @property
+    def width(self):
+        """return the value of the attribute width."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """sets the value of the private attribute width."""
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
