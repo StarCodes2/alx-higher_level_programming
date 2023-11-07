9aad504467a3678c2cf1355df14205744f087118
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-11-07 08:48:18 -0800

    To write string to a text file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a function that writes to a text file."""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a text file."""
+    with open(filename, mode="w", encoding="utf-8") as myfile:
+        myfile.write(text)
