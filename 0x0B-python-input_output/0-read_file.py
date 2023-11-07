535a88d9552f81f0f2b3b9039fe398547a8610a7
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-11-07 08:31:52 -0800

    To read read and print the content of a text file to stdout.

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """Reads a text file and prints its content to stdout."""
     with open(filename, encoding="utf-8") as myfile:
-        print(myfile.read())
+        print(myfile.read(), end="")
