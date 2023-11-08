315c97e3ad4069dd576be4766d952ca768075c5f
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-11-08 05:24:02 -0800

    extended the Student class by adding a filter to the to_json method

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defines class Student."""
+
+
+class Student:
+    """Defines 3 public instance attributes a function to initialise them
+    and a method that retrieves the dictionary representation of an
+    instance of Student."""
+    def __init__(self, first_name, last_name, age):
+        """Initialises the instance attributes."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns the dictionary representation of instance of Student."""
+        if type(attrs) == list and all(type(x) == str for x in attrs):
+            return {x: self.__dict__[x] for x in self.__dict__
+                    if x in attrs}
+        return self.__dict__
