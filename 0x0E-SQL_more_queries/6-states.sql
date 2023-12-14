c78940a92794a8836ab4c9e7a0eac72874ca0be0
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-12-13 23:21:48 -0800

    To create a database 'hbtn_0d_usa' and a table 'states' within the database with the primary key constraint and auto increment added to the id field

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,4 @@
+-- Creates a database and create table in the new database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256));
