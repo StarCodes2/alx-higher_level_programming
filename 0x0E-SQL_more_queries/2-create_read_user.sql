338d147924a047df4a23d1bed17f32aa912ca6ed
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-12-13 22:38:34 -0800

    File creates a database and user, adds a select privilege for the database to the user

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- Creates a database called hbtn_0d_2 and a user called user_0d_2 and grant
+-- user SELECT privilege to the database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
