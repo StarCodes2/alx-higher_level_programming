338b3c066f5f5dbdd7adbff834614e9da40f2379
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-12-14 14:57:49 -0800

    To display all the cities in California without ising join

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,5 @@
 -- Lists all the cities of California that can be found in the database
 SELECT id, name
 FROM cities
-WHERE id = (SELECT id FROM states WHERE name = 'California');
+WHERE state_id = (SELECT id FROM states WHERE name = 'California')
+ORDER BY id ASC;
