575ca4c34fa76d6dfd1994df889ce826361b8647
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-12-15 04:28:02 -0800

    Use the hbtn_0d_tvshows database to list all genres not linked to the show Dexter.

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,16 @@
+-- Uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter.
+SELECT DISTINCT tg.name
+FROM tv_genres AS tg
+INNER JOIN tv_show_genres AS tsg
+ON tg.id = tsg.genre_id
+INNER JOIN tv_shows AS ts
+ON tsg.show_id = ts.id
+WHERE tg.name NOT IN
+	(SELECT name
+	 FROM tv_genres AS tg
+		INNER JOIN tv_show_genres AS tsg
+		ON tg.id = tsg.genre_id
+		INNER JOIN tv_shows AS ts
+		ON tsg.show_id = ts.id
+	 WHERE ts.title = "Dexter")
+ORDER BY tg.name ASC;
