f1a1e5b80340550af215a2605ba8361aac67c3db
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-12-14 21:09:50 -0800

    To list all shows contained in hbtn_0d_tvshows that have at least one genre linked.

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- Imports a database dump from hbtn_0d_tvshows
 SELECT ts.title, tg.genre_id
 FROM tv_shows AS ts
-LEFT JOIN tv_show_genres AS tg
-ON ts.id = tg.genre_id
+INNER JOIN tv_show_genres AS tg
+ON ts.id = tg.show_id
 ORDER BY ts.title, tg.genre_id ASC;
