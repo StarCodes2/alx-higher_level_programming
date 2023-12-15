f5d63bdc9c02db40407f2c89341d8d864e841fdc
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-12-14 19:58:19 -0800

    To display all cities contained in a database

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- Prints all cities contained in the databet
 SELECT cities_id, cities.name, states.name
-from cities
+FROM cities
 LEFT JOIN states
-oN state_id = states.id
+ON state_id = states.id
 ORDER BY cities.id ASC;
