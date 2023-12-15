a6d7cbf6146e721f19c4fe907a0a50b67890ecbe
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-12-14 23:32:34 -0800

    To list all shows in the hbtn_0d_tvshows database

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- Lists all shows contained in the database hbtn_0d_tvshows.
+SELECT ts.title, tg.genre_id
+FROM tv_shows AS ts
+LEFT JOIN tv_show_genres AS tg
+ON ts.id = tg.show_id
+ORDER BY ts.title, tg.genre_id ASC;
