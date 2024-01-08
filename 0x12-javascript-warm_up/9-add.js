35ce954d22c28316532e6d970c830f7dc766371f
Ezekiel Ogunewu <anu.ezekiel02@gamil.com>
2024-01-08 23:40:35 +0000

    task 9

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const num1 = Math.floor(Number(process.argv[2]));
+const num2 = Math.floor(Number(process.argv[3]));
+
+console.log(add(num1, num2));
+
+function add(a, b) {
+  return a + b;
+}
