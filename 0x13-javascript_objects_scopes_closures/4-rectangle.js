bb0cd5f1e8f1371ae95154c523e53367ed69f10d
Ezekiel Ogunewu <anu.ezekiel02@gamil.com>
2024-01-09 22:42:52 +0000

    To switch the values of the instance attributes and double their values

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -13,4 +13,15 @@ module.exports = class Rectangle {
       console.log(line);
     }
   }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
 };
