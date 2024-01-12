81dfc808fd6509bfbe0499cd61d146937bfefa9b
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-01-12 04:47:41 -0800

    To list the content of the states table where name matches the argument

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -14,11 +14,13 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3], charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("SELECT * \
+                FROM states \
+                WHERE name='{}' \
+                ORDER BY id ASC".format(argv[4]))
     rows = cur.fetchall()
     for row in rows:
-        if row[1] == argv[4]:
-            print(row)
+        print(row)
 
     cur.close()
     conn.close()
