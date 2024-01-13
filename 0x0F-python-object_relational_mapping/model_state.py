6958e823e2f8316f4c35ccc58d4c2338d3d84aa6
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-01-13 01:57:36 -0800

    FEAT: SQLAlchemy State model definition

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines a State model that inherits from SQLAlchemy base and links to the
+   MgSQL table state."""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+
+
+class State(Base):
+    """Represents the state table for a MySQL Database
+
+    __tablename__ (str): Name of the table in the MySQL database to store state
+    id (sqlalchemy.Integer): State's id.
+    name (sqlachemy.String): State's name.
+    """
+    __tablename__ = "state"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
