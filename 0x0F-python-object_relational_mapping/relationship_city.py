05ec45593449a5449b0d72e45365f0749a154ebb
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-01-14 16:56:38 -0800

    task 15

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defines a City model that inherits from SQLAlchemy base and links to the
+   MgSQL table city."""
+from sqlalchemy import Column, Integer, ForeignKey, String
+from relationship_state.py import State
+from sqlalchemy.ext.declarative ext import declarative_base
+Base = declarative_base()
+
+
+class City(Base):
+    """Represents the cities table for a MySQL Database
+
+    __tablename__ (str): Name of the table in the MySQL database to store state
+    id (sqlalchemy.Integer): City's id.
+    name (sqlachemy.String): City's name.
+    """
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey(State.id), nullable=False)
---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defines a State model that inherits from SQLAlchemy base and links to the
+   MgSQL table state."""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from relationship_city import Base, City
+
+
+class State(Base):
+    """Represents the state table for a MySQL Database
+
+    __tablename__ (str): Name of the table in the MySQL database to store state
+    id (sqlalchemy.Integer): State's id.
+    name (sqlachemy.String): State's name.
+    cities (relationship): State - City relationship
+    """
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
