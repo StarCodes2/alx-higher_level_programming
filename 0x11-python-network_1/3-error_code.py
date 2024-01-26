07de54b74804bf6f1899a4d94c2af98524ef0434
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-01-26 15:49:03 -0800

    Send a request to the given URL and displays the body of the response

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" sends a request to a given URL and displays the body of the response"""
+from sys import argv
+from urllib.request import Request, urlopen
+import urllib.error
+
+
+if __name__ == "__main__":
+    request = Request(argv[1])
+    try:
+        with request.urlopen as response:
+            print(response.read().decode("utf-8"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
