020066ac8d693760ee1831670ccdaf4272d76d8e
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-01-27 02:56:18 -0800

    To send a request to a given URL and displays the value of the variable X-Request-Id in the response header

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Sends a request to a given URL and displays the value of the variable
+X-Request-Id in the response header
+
+Usage: ./5-hbtn_header.py <URL>
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    r = requests.get(argv[1])
+    print(r.headers["X-Request-Id"])
