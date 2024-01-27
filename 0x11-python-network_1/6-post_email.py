8cf15853fddbd4a9099880175dcaeb4aaed502fe
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-01-27 04:25:37 -0800

    To send a POST request to the passed URL with the email as a parameter, and finally displays the body of the response

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -11,4 +11,4 @@ from sys import argv
 if __name__ == "__main__":
     value = {'email': argv[2]}
     r = requests.post(argv[1], data=value)
-    print("Your email is: ".format(r.text))
+    print(r.text)
