42f4418a3fe757152de7ed083b135975d1f1238b
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-04-03 15:12:01 +0000

    To update the text color of the <header> element to red (#FF0000).

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,5 @@
+// Change the color of the header tag to red
+document.addEventListener('DOMContentLoaded', () => {
+  const header = document.querySelector('header');
+  header.style.color = '#FF0000';
+});
